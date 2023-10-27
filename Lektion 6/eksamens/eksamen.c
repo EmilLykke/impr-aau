6fe4a2e4b12a4c8dcd367e0e454eed873b706207
Emil Lykke <emillykkeg@gmail.com>
2023-10-27 08:06:11 +0200

    lektioner

--- Lektion 6/eksamens/eksamen.c
+++ Lektion 6/eksamens/eksamen.c
@@ -1,1 +1,136 @@
-BOISSLyoNCg0KTmFtZTogRW1pbCBMeWtrZSBHcmFubg0KRW1haWw6IGVncmFubjIzQHN0dWRlbnQuYWF1LmRrDQpFZHVjYXRpb246IFNvZnR3YXJlIChTVykNCkFzc2lnbm1lbnQ6IEVrc2FtZW5zIG9wZ2F2ZSAxDQpEZXNjcmlwdGlvbjoNCkNhbGN1bGF0ZXIgd2l0aCBkaWZmZXJlbnQgb3B0aW9uLiBCb3RoIHRha2luZyB1bmFyeSBhbmQgYmluYXJ5IG9wZXJhdG9ycy4NClRoZSBjYWxjdWxhdGVyIGlzIHBlcmZvbWluZyB0aGUgYWN0aW9uIHRoZSB0aGUgYWNjdW11bGF0b3IuDQpUaGUgZmluYWwgYWNjdW11bGF0b3IgaXMgcHJpbnRlZCB3aGVuIHRoZSB1c2VyIGV4aXRzIHRoZSBwcm9ncmFtIHdpdGggJ3EnLg0KDQoqLw0KDQojaW5jbHVkZSA8c3RkaW8uaD4NCiNpbmNsdWRlIDxtYXRoLmg + DQoNCnZvaWQgc2Nhbl9kYXRhKGNoYXIgKm9wZXJhdG9yLCBkb3VibGUgKiBvcGVyYW5kLCBpbnQgKmlzVW5hcnkpOw0Kdm9pZCBkb19uZXh0X29wKGNoYXIgKm9wZXJhdG9yLCBkb3VibGUgKiBvcGVyYW5kLCBkb3VibGUgKmFjY3VtdWxhdG9yLCBpbnQgKmlzVW5hcnkpOw0Kdm9pZCBydW5fY2FsY3VsYXRvcihkb3VibGUgKmFjY3VtdWxhdG9yKTsNCnZvaWQgcHJpbnRfaGVscCgpOw0KDQppbnQgbWFpbih2b2lkKQ0Kew0KICAgIGRvdWJsZSBhY2N1bXVsYXRvciA9IDAuMDsNCiAgICBydW5fY2FsY3VsYXRvcigmYWNjdW11bGF0b3IpOw0KDQogICAgcHJpbnRmKCJGaW5hbCByZXN1bHQgaXM6ICVsZlxuIiwgYWNjdW11bGF0b3IpOw0KDQogICAgcmV0dXJuIDA7DQp9DQoNCnZvaWQgcnVuX2NhbGN1bGF0b3IoZG91YmxlICphY2N1bXVsYXRvcikNCnsNCiAgICBjaGFyIG9wZXJhdG9yOw0KICAgIGRvdWJsZSBvcGVyYW5kOw0KICAgIGludCBpc1VuYXJ5ID0gMDsNCg0KICAgIHdoaWxlIChvcGVyYXRvciE9ICdxJykNCiAgICB7DQogICAgICAgIHNjYW5fZGF0YSgmb3BlcmF0b3IsICYgb3BlcmFuZCwgJmlzVW5hcnkpOw0KICAgICAgICBkb19uZXh0X29wKCZvcGVyYXRvciwgJiBvcGVyYW5kLCBhY2N1bXVsYXRvciwgJmlzVW5hcnkpOw0KICAgICAgICBwcmludGYoIlJlc3VsdCBzbyBmYXI6ICVsZlxuIiwgKmFjY3VtdWxhdG9yKTsNCiAgICAgICAgaXNVbmFyeSA9IDA7DQogICAgfQ0KfQ0KDQp2b2lkIHNjYW5fZGF0YShjaGFyICpvcGVyYXRvciwgZG91YmxlICogb3BlcmFuZCwgaW50ICppc1VuYXJ5KQ0Kew0KICAgIHByaW50ZigiRW50ZXIgb3BlcmF0b3IgKCdoJyBmb3IgaGVscCk6ICIpOw0KICAgIHNjYW5mKCIgJWMiLCBvcGVyYXRvcik7DQogICAgaWYgKCpvcGVyYXRvcj09ICdoJykNCiAgICB7DQogICAgICAgIHByaW50X2hlbHAoKTsNCiAgICB9DQogICAgZWxzZSBpZiAoKm9wZXJhdG9yPT0gJyMnIHx8ICogb3BlcmF0b3I9PSAnIScgfHwgKiBvcGVyYXRvcj09ICclJyB8fCAqIG9wZXJhdG9yPT0gJ3EnKQ0KICAgIHsNCiAgICAgICAgKmlzVW5hcnkgPSAxOw0KICAgICAgICAqb3BlcmFuZCA9IDAuMDsNCiAgICB9DQogICAgZWxzZSBpZiAoKm9wZXJhdG9yPT0gJysnIHx8ICogb3BlcmF0b3I9PSAnLScgfHwgKiBvcGVyYXRvcj09ICcqJyB8fCAqIG9wZXJhdG9yPT0gJy8nIHx8ICogb3BlcmF0b3I9PSAnXicpDQogICAgew0KICAgICAgICBwcmludGYoIkVudGVyIG9wZXJhbmQ6ICIpOw0KICAgICAgICBzY2FuZigiICVsZiIsIG9wZXJhbmQpOw0KICAgIH0NCiAgICBlbHNlDQogICAgew0KICAgICAgICBwcmludGYoIklucHV0IHdhcyBub3QgYW4gb3B0aW9uLCB2aWV3ICdoJyBmb3IgaGVscC5cbiIpOw0KICAgICAgICBwcmludGYoIlRyeSBhZ2Fpbi5cbiIpOw0KICAgICAgICBzY2FuX2RhdGEob3BlcmF0b3IsIG9wZXJhbmQsIGlzVW5hcnkpOw0KICAgIH0NCn0NCg0Kdm9pZCBkb19uZXh0X29wKGNoYXIgKm9wZXJhdG9yLCBkb3VibGUgKiBvcGVyYW5kLCBkb3VibGUgKmFjY3VtdWxhdG9yLCBpbnQgKmlzVW5hcnkpDQp7DQogICAgaWYgKCppc1VuYXJ5KQ0KICAgIHsNCiAgICAgICAgc3dpdGNoICgqb3BlcmF0b3IpDQogICAgICAgIHsNCiAgICAgICAgY2FzZSAnIyc6DQogICAgICAgICAgICAqYWNjdW11bGF0b3IgPSAqYWNjdW11bGF0b3IgPCAwID8gKmFjY3VtdWxhdG9yIDogc3FydCgqYWNjdW11bGF0b3IpOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGNhc2UgJyUnOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yICo9IC0xOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGNhc2UgJyEnOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yID0gKmFjY3VtdWxhdG9yID09IDAgPyAqYWNjdW11bGF0b3IgOiAxIC8gKCphY2N1bXVsYXRvcik7DQogICAgICAgICAgICBicmVhazsNCiAgICAgICAgZGVmYXVsdDoNCiAgICAgICAgICAgIGJyZWFrOw0KICAgICAgICB9DQogICAgfQ0KICAgIGVsc2UNCiAgICB7DQogICAgICAgIHN3aXRjaCAoKm9wZXJhdG9yKQ0KICAgICAgICB7DQogICAgICAgIGNhc2UgJysnOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yICs9ICpvcGVyYW5kOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGNhc2UgJy0nOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yIC09ICpvcGVyYW5kOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGNhc2UgJyonOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yICo9ICpvcGVyYW5kOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGNhc2UgJy8nOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yIC89ICpvcGVyYW5kOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGNhc2UgJ14nOg0KICAgICAgICAgICAgKmFjY3VtdWxhdG9yID0gcG93KCphY2N1bXVsYXRvciwgKm9wZXJhbmQpOw0KICAgICAgICAgICAgYnJlYWs7DQogICAgICAgIGRlZmF1bHQ6DQogICAgICAgICAgICBicmVhazsNCiAgICAgICAgfQ0KICAgIH0NCn0NCg0Kdm9pZCBwcmludF9oZWxwKCkNCnsNCiAgICBwcmludGYoIlxuQmluYXJ5IG9wZXJhdG9ycyB0byB1c2U6XG4iKTsNCiAgICBwcmludGYoIisgICAgZm9yIGFkZGl0aW9uIG9mIHRoZSBhY2N1bXVsYXRvciB3aXRoIHRoZSBvcGVyYW5kIGZvclxuIik7DQogICAgcHJpbnRmKCItICAgIGZvciBzdWJ0cmFjdGlvbiBvZiB0aGUgYWNjdW11bGF0b3Igd2l0aCB0aGUgb3BlcmFuZFxuIik7DQogICAgcHJpbnRmKCIqICAgIGZvciBtdWx0aXBsaWNhdGlvbiBvZiB0aGUgYWNjdW11bGF0b3Igd2l0aCB0aGUgb3BlcmFuZFxuIik7DQogICAgcHJpbnRmKCIvICAgIGZvciBkaXZpc2lvbiBvZiB0aGUgYWNjdW11bGF0b3IgYnkgdGhlIG9wZXJhbmRcbiIpOw0KICAgIHByaW50ZigiXiAgICBmb3IgcmFpc2luZyB0aGUgYWNjdW11bGF0b3IgdG8gdGhlIHBvd2VyIG9mIHRoZSBvcGVyYW5kXG4iKTsNCiAgICBwcmludGYoIi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tXG4iKTsNCiAgICBwcmludGYoIlVuYXJ5IG9wZXJhdG9ycyB0byB1c2U6XG4iKTsNCiAgICBwcmludGYoIiMgICAgZm9yIHRoZSBzcXVhcmUgcm9vdCBvZiB0aGUgYWNjdW11bGF0b3JcbiIpOw0KICAgIHByaW50ZigiJSUgICB0byBjaGFuZ2UgdGhlIHNpZ24gb2YgdGhlIGFjY3VtdWxhdG9yXG4iKTsNCiAgICBwcmludGYoIiEgICAgdG8gZGl2aWRlIDEgYnkgdGhlIGFjY3VtdWxhdG9yXG4iKTsNCiAgICBwcmludGYoInEgICAgdG8gcXVpdCB0aGUgY2FsY3VsYXRvciB3aXRoIHRoZSBmaW5hbCByZXN1bHRcbiIpOw0KICAgIHByaW50ZigiLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS1cblxuIik7DQp9
+/*
+
+Name: Emil Lykke Grann
+Email: egrann23@student.aau.dk
+Education: Software (SW)
+Assignment: Eksamens opgave 1
+Description:
+Calculater with different option. Both taking unary and binary operators.
+The calculater is perfoming the action the the accumulator.
+The final accumulator is printed when the user exits the program with 'q'.
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void scan_data(char *operator, double * operand, int *isUnary);
+void do_next_op(char *operator, double * operand, double *accumulator, int *isUnary);
+void run_calculator(double *accumulator);
+void print_help();
+
+int main(void)
+{
+    double accumulator = 0.0;
+    run_calculator(&accumulator);
+
+    printf("Final result is: %lf\n", accumulator);
+
+    return 0;
+}
+
+void run_calculator(double *accumulator)
+{
+    char operator;
+    double operand;
+    int isUnary = 0;
+
+    while (operator!= 'q')
+    {
+        scan_data(&operator, & operand, &isUnary);
+        do_next_op(&operator, & operand, accumulator, &isUnary);
+        printf("Result so far: %lf\n", *accumulator);
+        isUnary = 0;
+    }
+}
+
+//  Scans the data (operator, operand) from the user
+void scan_data(char *operator, double * operand, int *isUnary)
+{
+    printf("Enter operator ('h' for help): ");
+    scanf(" %c", operator);
+    if (*operator== 'h')
+    {
+        print_help();
+    }
+    else if (*operator== '#' || * operator== '!' || * operator== '%' || * operator== 'q')
+    {
+        *isUnary = 1;
+        *operand = 0.0;
+    }
+    else if (*operator== '+' || * operator== '-' || * operator== '*' || * operator== '/' || * operator== '^')
+    {
+        printf("Enter operand: ");
+        scanf(" %lf", operand);
+    }
+    else
+    {
+        printf("Input was not an option, view 'h' for help.\n");
+        printf("Try again.\n");
+        scan_data(operator, operand, isUnary);
+    }
+}
+
+// Does the next operation
+void do_next_op(char *operator, double * operand, double *accumulator, int *isUnary)
+{
+    if (*isUnary)
+    {
+        switch (*operator)
+        {
+        case '#':
+            // makes sure we do not take sqrt of negative number
+            *accumulator = *accumulator < 0 ? *accumulator : sqrt(*accumulator);
+            break;
+        case '%':
+            *accumulator *= -1;
+            break;
+        case '!':
+            // makes sure we do not devide by 0
+            *accumulator = *accumulator == 0 ? *accumulator : 1 / (*accumulator);
+            break;
+        default:
+            break;
+        }
+    }
+    else
+    {
+        switch (*operator)
+        {
+        case '+':
+            *accumulator += *operand;
+            break;
+        case '-':
+            *accumulator -= *operand;
+            break;
+        case '*':
+            *accumulator *= *operand;
+            break;
+        case '/':
+            *accumulator /= *operand;
+            break;
+        case '^':
+            *accumulator = pow(*accumulator, *operand);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void print_help()
+{
+    printf("\nBinary operators to use:\n");
+    printf("+    for addition of the accumulator with the operand for\n");
+    printf("-    for subtraction of the accumulator with the operand\n");
+    printf("*    for multiplication of the accumulator with the operand\n");
+    printf("/    for division of the accumulator by the operand\n");
+    printf("^    for raising the accumulator to the power of the operand\n");
+    printf("-----------------------------\n");
+    printf("Unary operators to use:\n");
+    printf("#    for the square root of the accumulator\n");
+    printf("%%   to change the sign of the accumulator\n");
+    printf("!    to divide 1 by the accumulator\n");
+    printf("q    to quit the calculator with the final result\n");
+    printf("-----------------------------\n\n");
+}
--- Lektion 7/ppm-workshop/ppm-workshop/diagonal-prog.c
+++ Lektion 7/ppm-workshop/ppm-workshop/diagonal-prog.c
@@ -4,14 +4,16 @@
 #include <stdio.h>
 #include "ppm.h"
 
-int main(void) {
+int main(void)
+{
 
-  ppm *img = make_image(500, 500,  make_pixel(255U, 255U, 255U));  // white
+  ppm *img = make_image(500, 500, make_pixel(255U, 255U, 255U)); // white
   int i;
 
   pixel black_pixel = make_pixel(0U, 0U, 0U);
 
-  for(i = 0; i < 500; i++){
+  for (i = 0; i < 500; i++)
+  {
     set_pixel(img, i, i, black_pixel);
     set_pixel(img, 500 - i - 1, i, black_pixel);
   }
